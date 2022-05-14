7b0a1323c52a9bbd5b4b25a9a07af17270409b97
cjpoulsen <53880342+cjpoulsen@users.noreply.github.com>
2022-05-14 15:59:45 -0500

    Update curl url

--- scripts/get-grammar.sh
+++ scripts/get-grammar.sh
@@ -5,10 +5,9 @@ grammarVersion="0.0.1"
 domain="https://raw.githubusercontent.com"
 organization="merideum"
 repository="merit"
-path="src/main/resources"
 grammarFile="Merit.g4"
 
-url="$domain/$organization/$repository/$grammarVersion/$path/$grammarFile"
+url="$domain/$organization/$repository/$grammarVersion/$grammarFile"
 
 echo "Pulling $grammarFile from $url"
 echo ""
